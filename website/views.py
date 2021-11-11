2e9e89ff5863b5393cf82dcb5ee31a108c7f8bc4
kejlor <woj450@gmail.com>
2021-11-11 16:13:51 +0100

    Cleaned code. Added more accurate input description for user inside index.html

--- website/views.py
+++ website/views.py
@@ -3,14 +3,21 @@ from flask import render_template, request, Blueprint
 
 views = Blueprint('views', __name__)
 
+BASE = "https://api.opendota.com/api/"
+
 
 @views.route('/history', methods=['POST'])
 def get_match_history():
     steam_id = request.form['steam_id']
     r = requests.get(
-        'https://api.opendota.com/api/players/' + steam_id + '/matches')
+        BASE + 'players/' + steam_id + '/matches')
     match_list = r.json()
     return render_template('history.html', match_list=match_list)
+
+
+@views.route('/match-details', methods=['POST'])
+def get_match_details():
+    pass
 
 
 @views.route('/')
