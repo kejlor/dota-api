e4bfb65692a873665f9209f98612b999d12d4935
kejlor <woj450@gmail.com>
2021-11-11 16:48:29 +0100

    Created making links inside history.html routing user to match details info page. Added match_details.html template. Added new routing function which parses JSON data.

--- website/views.py
+++ website/views.py
@@ -9,15 +9,17 @@ BASE = "https://api.opendota.com/api/"
 @views.route('/history', methods=['POST'])
 def get_match_history():
     steam_id = request.form['steam_id']
-    r = requests.get(
+    user_request = requests.get(
         BASE + 'players/' + steam_id + '/matches')
-    match_list = r.json()
+    match_list = user_request.json()
     return render_template('history.html', match_list=match_list)
 
 
-@views.route('/match-details', methods=['POST'])
-def get_match_details():
-    pass
+@views.route('/match-details/<match_id>', methods=['GET'])
+def get_match_details(match_id):
+    user_request = requests.get(BASE + '/matches/' + match_id)
+    match_details = user_request.json()
+    return render_template('match_details.html', match_details=match_details)
 
 
 @views.route('/')
